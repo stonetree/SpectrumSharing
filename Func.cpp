861dbbebe4bccb2e0d6b9c9235ec9bf588f64dc3
Liu <liuliu.sky@gmail.com>
2013-10-29 20:10:35 -0400

    Modified the mis-alignment function to make the link just to be calculated up to one time (calculateMisAlignmentCount_Moficied) instead of multiple times that used in calculateMisAlignmentCount function.

--- Func.cpp
+++ Func.cpp
@@ -522,6 +522,9 @@ int obtainAvailblePathSlot(cTopology& _topo, cRequest* _request,vector<ID>& _pat
 
 int calculateMisAlignCount_Modified(cTopology& _topo,cRequest* _request,vector<ID>& _path,int _slot)
 {
+	//when calculate the count of misalignment we just count each link up to one time instead of the manner of counting multiple times that 
+	//used in "calculateMisAlignment" function.
+	
 	vector<ID>::iterator iter_nodeID;
 
 	vector<ID> linkID_vec;
@@ -682,7 +685,7 @@ int obtainMinPathObjective(cTopology& _topo,cRequest* _request,vector<ID>& _path
 			spectrumUsed += _topo.phyLink_vec[*iter_linkID - 1].calculateSepctrumUsed(_request,_path_class,*iter_slot);
 		}	
 
-		misAlignmentCount = calculateMisAlignCount(_topo,_request,_path,*iter_slot);
+		misAlignmentCount = calculateMisAlignCount_Modified(_topo,_request,_path,*iter_slot);
 
 		
 		objective = calculateObjective(fragmentationCount,misAlignmentCount,spectrumUsed,unUsedSlot,linkID_vec.size());
